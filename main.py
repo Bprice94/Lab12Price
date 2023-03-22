ef531f36f5a671a4adf641f6f4272c008dea2f8a
bprice2 <126625061+Bprice94@users.noreply.github.com>
2023-03-22 06:50:21 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+## Generate 3 random numbers
+# assign to the list 'numbers'
+i = 2
+import random
+numbers = [0] * 3
+while i < 3:
+  numbers [i] = random.randint(0, 100)
+  i += 1
+  num1 = 'smallest interger'
+  ret = num1.find('smallest interger')
+  print (numbers)
+  print (ret) 
+
